4e48a0788285587b77f2d8ea08e72b627190bb78
yash <yashrajsinhjadeja2253@gmail.com>
2026-03-17 14:48:57 +0530

    App works

--- src/screens/DoctorScreen.tsx
+++ src/screens/DoctorScreen.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 //Component
 import DoctorList from '../components/DoctorList/DoctorList';
-import { StatusBar } from 'react-native';
+
 
 export default function DoctorScreen() {
   return <DoctorList />;
